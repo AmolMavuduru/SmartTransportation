3bddf34a823208de0235644a9268885ca2f59400
Prakash Mavuduru <amol@prakashs-mbp>
2016-09-26 18:57:57 -0500

    Initial Commit

---
+++ SmartTransportation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmartTransportation
+//
+//  Created by Amol on 9/26/16.
+//  Copyright © 2016 Amol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SmartTransportation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SmartTransportation
+//
+//  Created by Amol on 9/26/16.
+//  Copyright © 2016 Amol. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
